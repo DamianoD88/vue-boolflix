bf02e835bd7b673a936d46c923bc1689ce3cdd0e
DamianoD88 <dispe88@live.it>
2021-07-13 16:00:11 +0200

    add scheletro

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+      <div class="container">
+          <div class="logo">
+              Boolflix
+          </div>
+          <input type="text"
+          placeholder="Search Movies"
+          v-model="searchText"
+          @keyup="$emit('ricerca', searchText)">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            searchText: ''
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/style/utilities";
+@import "../styles/vars";
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+      <div class="search">
+          <h2>Risultato ricerca: </h2>
+          <div class="cards">
+              <Card />
+          </div>
+      </div>
+  </main>
+</template>
+
+<script>
+
+import Card from "@/components/Card.vue";
+
+export default {
+    name: "Main",
+    components: {
+        Card,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/style/utilities";
+@import "../styles/vars";
+
+
+</style>
